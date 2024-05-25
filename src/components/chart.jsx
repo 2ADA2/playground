0f2040da3951a251fbcc16488bc5279ce45a1daa
Yauheni <160595384+tpyauheni@users.noreply.github.com>
2024-05-25 11:11:05 +0300

    playground v1.0.3

--- src/components/chart.jsx
+++ src/components/chart.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import {Line} from "react-chartjs-2";
-import {CategoryScale, Chart, LinearScale, LineElement, PointElement} from "chart.js";
+import {CategoryScale, Chart, LinearScale, LineElement, PointElement, Tooltip} from "chart.js";
 
 Chart.register(CategoryScale);
 Chart.register(LinearScale);
 Chart.register(PointElement);
 Chart.register(LineElement);
+Chart.register(Tooltip);
 
 
 export const LineChart = ({data, data2 = 0}) => {
@@ -27,15 +28,19 @@ export const LineChart = ({data, data2 = 0}) => {
             }),
             datasets: [
                 {
+                    fill: true,
+                    label:"время",
                     data: data || [],
                     borderColor: "rgba(51,229,255,0.76)",
                     lineTension: 0.5
                 },
                 {
+                    label:"мины",
+                    fill: true,
                     data: data2 || [],
                     borderColor: "#ff33dd",
                     lineTension: 0.5,
-                    borderWidth: 5
+                    borderWidth: 3
                 }
             ]
         };
@@ -44,9 +49,11 @@ export const LineChart = ({data, data2 = 0}) => {
             labels: data.map(a => a),
             datasets: [
                 {
+                    label:"счет",
                     data: data || [],
                     borderColor: "#68ff6b",
-                    lineTension: 0.5
+                    lineTension: 0.5,
+                    fill:true,
                 }
             ]
         };
@@ -58,19 +65,17 @@ export const LineChart = ({data, data2 = 0}) => {
             width={160}
             height={60}
             options={{
-                animation: false,
-                title: {
-                    display: false,
-                    text: "Рекорд",
-                    fontSize: 20
-                },
+                animation:false,
                 legend: {
-                    display: false,
+                    display: false
                 },
                 scales: {
-                    y: {
-                        min: 0
+                    y:{
+                        min:0,
                     }
+                },
+                plugins:{
+                    legend:true,
                 }
 
             }}
